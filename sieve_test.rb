cc850c213fcfd2af2f748a53154152db1e787927
Nathan Menge <mengen@upmc.edu>
2014-11-06 10:32:53 -0500

    Updated tests and added Gemfile

--- sieve_test.rb
+++ sieve_test.rb
@@ -2,17 +2,15 @@ require "minitest/autorun"
 require './sieve_of_eratosthenes'
 
 class SieveTest < MiniTest::Test
-  def test_factor
+  def test_factor?
     s = SieveOfEratosthenes.new(6)
-    assert !s.factor(1,1)
-    assert !s.factor(nil,5)
-    assert !s.factor(5,nil)
-    assert s.factor(2,4)
-    assert !s.factor(3,5)
+    assert s.factor?(2,2)
+    assert s.factor?(2,4)
+    assert !s.factor?(3,5)
   end
 
   def test_find_primes
-    s = SieveOfEratosthenes.new(6)
-    assert_equal [2,3,5], s.find_primes
+    s = SieveOfEratosthenes.new(30)
+    assert_equal [2,3,5,7,11,13,17,19,23,29], s.find_primes
   end
 end
