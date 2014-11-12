3576f4aba89a8486eea644de016b5a19ba7e747d
Nathan Menge <mengen@upmc.edu>
2014-11-11 22:47:45 -0500

    Enhanced the test to verify the output is prime by using the Prime class in Ruby.

--- sieve_test.rb
+++ sieve_test.rb
@@ -1,9 +1,10 @@
-require "minitest/autorun"
+require 'minitest/autorun'
+require 'prime'
 require './sieve_of_eratosthenes'
 
 class SieveTest < MiniTest::Test
   def test_find_primes
-    s = SieveOfEratosthenes.new(40)
-    assert_equal [2,3,5,7,11,13,17,19,23,29,31,37], s.find_primes
+    s = SieveOfEratosthenes.new(100000)
+    assert_equal s.find_primes, Prime.first(9592)
   end
 end
